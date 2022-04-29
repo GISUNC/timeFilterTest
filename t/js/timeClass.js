2c765dfe1694f2c5fe98c97c3a4a50776415551f
jarce <jarce@liveunc.edu>
2022-04-29 10:49:15 -0400

    stop scrolling class eliminated

--- t/js/timeClass.js
+++ t/js/timeClass.js
@@ -686,12 +686,12 @@ var TF = { //time filters to manage tiles of different years
                         onMouseDown: function(evt){
                                 elEvt = evt
                                 this.disableScroll();
-                                var daBody = document.getElementsByTagName('body')
-                                var l = daBody.length; // this will stop the scrolling in cellphones
-                                for (var i=0; i < l; i++){daBody[i].className = 'stop-scrolling'}   
-                                var daBody = document.getElementsByTagName('html')
-                                var l = daBody.length; // this will stop the scrolling in cellphones
-                                for (var i=0; i < l; i++){daBody[i].className = 'stop-scrolling'}  
+                                // var daBody = document.getElementsByTagName('body')
+                                // var l = daBody.length; // this will stop the scrolling in cellphones
+                                // for (var i=0; i < l; i++){daBody[i].className = 'stop-scrolling'}   
+                                // var daBody = document.getElementsByTagName('html')
+                                // var l = daBody.length; // this will stop the scrolling in cellphones
+                                // for (var i=0; i < l; i++){daBody[i].className = 'stop-scrolling'}  
                                 if (evt.target.closest('.svgpapa') == null){return}
                                 this.svgParent = evt.target.closest('.svgpapa')
                                 const contDiv = document.getElementById(evt.target.classList[1])
